50ec70324e48353aa747bb184b38b8d05c912711
DC <pc911bklyn@gmail.com>
2021-02-09 12:52:37 -0500

    Class Staff uses a HoursWorked {get; set; } shortcut

--- ClassDemo/Program.cs
+++ ClassDemo/Program.cs
@@ -10,32 +10,33 @@ namespace ClassDemo
             Console.WriteLine("---------------------------");
         }
         public Staff(string firstName, string lastName) {
-            nameOfStaff = firstName + lastName;
+            nameOfStaff = firstName + " " + lastName;
             Console.WriteLine("Full name of a created object: " + nameOfStaff);
         }
         public string nameOfStaff;
         private const int hourlyRate = 30;
         private int hWorked;
-        public int HoursWorked
-        {
-            get
-            {
-                return hWorked;
-            }
-            set
-            {
-                if (value > 0) hWorked = value;
-                else hWorked = 0;
-            }
-        } 
-        //public int HoursWorked { get; set; }
+        //public int HoursWorked
+        //{
+        //    get
+        //    {
+        //        return hWorked;
+        //    }
+        //    set
+        //    {
+        //        if (value > 0) hWorked = value;
+        //        else hWorked = 0;
+        //    }
+        //}
+        public int HoursWorked { get; set; }
         public void PrintMessage()
         {
             Console.WriteLine("Calculating Pay...");
         }
-        public int CalculatePay ()
+        public int CalculatePay()
         {
-            int staffPay = hWorked * hourlyRate;
+            //int staffPay = hWorked * hourlyRate;
+            int staffPay = HoursWorked * hourlyRate;
             PrintMessage();
             return staffPay;
         }
@@ -49,17 +50,25 @@ namespace ClassDemo
             return "Name of Staff: " + nameOfStaff + "hourly rate: " +
                 hourlyRate + " hours worked: " + hWorked;
         }
-    }
+    } //end of class Staff
+
+    //static class printMessage {
+    //    static int number = 10;
+    //    static public printNumber {
+    //        //dslfjdslk
+    //    }
+
+    //}
     class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
+                       //Console.WriteLine("Hello World!");
+
             Staff dennis = new Staff("Dennis", "Rodman");
-            dennis.HoursWorked = 15;
+            dennis.HoursWorked = 20;
+            Console.WriteLine("Dennis' hours worked: " + dennis.HoursWorked);
             Console.WriteLine("Dennis' pay: " + dennis.CalculatePay());
-        }     
-    }
-
-
-}
+        } //end of method Main
+    } //end of class Program
+} // end of namespace class Demo
